38b6bce3982369af0654828faceaa047d1b3ce08
Sadik Ay <sadikay91@gmail.com>
2017-03-07 18:49:03 +0300

    add email sent at field and filter option

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -56,7 +56,7 @@ class CampaignsController < ApplicationController
         campaign_user = @campaign.campaign_users.find_by(user_id: _user.id)
         if campaign_user.draft?
           UserMailer.campaign_email(campaign_user).deliver_now
-          campaign_user.processed!
+          campaign_user.update(sent_at: Time.now, status: :processed)
         end
       rescue => e
         Rails.logger.info("MAILER EXCEPTION: #{e} - ID: #{_user.id}")
