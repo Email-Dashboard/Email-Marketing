807286187ca855d96885a651c43f7a97f8053e19
Sadik Ay <sadikay91@gmail.com>
2017-03-08 12:49:50 +0300

    fix dropdown and tag edit btn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require bootstrap
 //= require editable/bootstrap-editable
 //= require editable/rails
 //= require select2-full
