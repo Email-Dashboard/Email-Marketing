e47e3312191114220529f85053db2874dedd4c47
Sadik Ay <sadikay91@gmail.com>
2017-03-08 16:46:47 +0300

    add tilt template to campaign show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
