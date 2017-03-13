3b156b54c9ee1e7991cf00d4bb04be74ba64000b
Sadik Ay <sadikay91@gmail.com>
2017-03-13 17:57:33 +0300

    minimize sql quering

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -20,43 +20,8 @@ class Campaign < ApplicationRecord
 
   validates :name, :email_template, :account, presence: true
 
-  def draft_precent
-    (campaign_users.draft.count / campaign_users.count) * 100.0
-  end
-
-  def processed_precent
-    (campaign_users.processed.count / campaign_users.count) * 100.0
-  end
-
-  def dropped_precent
-    (campaign_users.dropped.count / campaign_users.count) * 100.0
-  end
-
-  def delivered_precent
-    (campaign_users.delivered.count / campaign_users.count) * 100.0
-  end
-
-  def deferred_precent
-    (campaign_users.deferred.count / campaign_users.count) * 100.0
-  end
-
-  def bounce_precent
-    (campaign_users.bounce.count / campaign_users.count) * 100.0
-  end
-
-  def open_precent
-    (campaign_users.open.count / campaign_users.count) * 100.0
-  end
-
-  def click_precent
-    (campaign_users.click.count / campaign_users.count) * 100.0
-  end
-
-  def spamreport_precent
-    (campaign_users.spamreport.count / campaign_users.count) * 100.0
-  end
-
-  def unsubscribe_precent
-    (campaign_users.unsubscribe.count / campaign_users.count) * 100.0
+  def self.calculate_percent(status_count, total_users_count)
+    return 0.0 if status_count.nil?
+    '%.2f' % ((status_count.to_f / total_users_count) * 100.0)
   end
 end
