15a65e6bb7860645af250aaef8c31dd0e75fdb8d
Sadik Ay <sadikay91@gmail.com>
2017-03-13 16:40:30 +0300

    Show stats by percent in campaign detail fixes #2

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -19,4 +19,44 @@ class Campaign < ApplicationRecord
   acts_as_taggable # Alias for campaign tags
 
   validates :name, :email_template, :account, presence: true
+
+  def draft_precent
+    (campaign_users.draft.count / campaign_users.count) * 100.0
+  end
+
+  def processed_precent
+    (campaign_users.processed.count / campaign_users.count) * 100.0
+  end
+
+  def dropped_precent
+    (campaign_users.dropped.count / campaign_users.count) * 100.0
+  end
+
+  def delivered_precent
+    (campaign_users.delivered.count / campaign_users.count) * 100.0
+  end
+
+  def deferred_precent
+    (campaign_users.deferred.count / campaign_users.count) * 100.0
+  end
+
+  def bounce_precent
+    (campaign_users.bounce.count / campaign_users.count) * 100.0
+  end
+
+  def open_precent
+    (campaign_users.open.count / campaign_users.count) * 100.0
+  end
+
+  def click_precent
+    (campaign_users.click.count / campaign_users.count) * 100.0
+  end
+
+  def spamreport_precent
+    (campaign_users.spamreport.count / campaign_users.count) * 100.0
+  end
+
+  def unsubscribe_precent
+    (campaign_users.unsubscribe.count / campaign_users.count) * 100.0
+  end
 end
