765243da6fd6c3b71bb5b055d6c71f9774a9cff3
Muhammet Dilek <dilekmuhammet@gmail.com>
2017-03-13 18:48:33 -0700

    uncomment_monkey

--- config/initializers/enqueuing_monkey.rb
+++ config/initializers/enqueuing_monkey.rb
@@ -1,15 +1,15 @@
-module ActiveJob
-  module Enqueuing
-    extend ActiveSupport::Concern
-
-    module ClassMethods
-      def perform_later(*args)
-        if Rails.env.production?
-          job_or_instantiate(*args).enqueue
-        else
-          job_or_instantiate(*args).perform_now
-        end
-      end
-    end
-  end
-end
+# module ActiveJob
+#   module Enqueuing
+#     extend ActiveSupport::Concern
+#
+#     module ClassMethods
+#       def perform_later(*args)
+#         if Rails.env.production?
+#           job_or_instantiate(*args).enqueue
+#         else
+#           job_or_instantiate(*args).perform_now
+#         end
+#       end
+#     end
+#   end
+# end
