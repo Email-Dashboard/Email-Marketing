62d4953dc2fde949c4d6a1376ac25e54f2b024f0
Sadik Ay <sadikay91@gmail.com>
2017-03-17 19:24:05 +0300

    fix duplication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def ransack_query_with_limit
+    if params[:limit_count].present?
+      @q.result(distinct: true).limit(params[:limit_count])
+    else
+      @q.result(distinct: true).page(params[:page])
+    end
+  end
 end
