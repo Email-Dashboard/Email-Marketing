d72bec586855d7726ee9ffa5b3a1775cff85b843
Sadik Ay <sadikay91@gmail.com>
2017-05-25 22:22:40 +0300

    fix multitenant uniqness

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525165610) do
+ActiveRecord::Schema.define(version: 20170525191634) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -122,7 +122,7 @@ ActiveRecord::Schema.define(version: 20170525165610) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["account_id"], name: "index_users_on_account_id"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end
