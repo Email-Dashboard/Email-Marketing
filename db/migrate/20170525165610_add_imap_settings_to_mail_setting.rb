4222129cadecc8af47e3c64841a9784d2b1f6c53
Sadik Ay <sadikay91@gmail.com>
2017-05-25 20:00:28 +0300

    add imap fields to mail_settings

---
+++ db/migrate/20170525165610_add_imap_settings_to_mail_setting.rb
@@ -0,0 +1,8 @@
+class AddImapSettingsToMailSetting < ActiveRecord::Migration[5.0]
+  def change
+    add_column :mail_settings, :imap_address, :string
+    add_column :mail_settings, :imap_port, :string
+    add_column :mail_settings, :imap_user_name, :string
+    add_column :mail_settings, :imap_password, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314051236) do
+ActiveRecord::Schema.define(version: 20170525165610) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -71,10 +71,14 @@ ActiveRecord::Schema.define(version: 20170314051236) do
     t.string   "domain"
     t.string   "user_name"
     t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.string   "provider"
     t.string   "reply_to"
+    t.string   "imap_address"
+    t.string   "imap_port"
+    t.string   "imap_user_name"
+    t.string   "imap_password"
     t.index ["account_id"], name: "index_mail_settings_on_account_id"
   end
 
