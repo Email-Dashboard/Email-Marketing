bb57169da0bf0fdfa0632b27dfb7073a6c6ec3cf
Sadik Ay <sadikay91@gmail.com>
2017-07-03 17:39:21 +0300

    add imap settings views

--- app/controllers/imap_settings_controller.rb
+++ app/controllers/imap_settings_controller.rb
@@ -1,6 +1,6 @@
-class SmtpSettingsController < ApplicationController
+class ImapSettingsController < ApplicationController
   before_action :authenticate_account!
-  before_action :set_imap_setting, only: [:edit, :update, :destroy, :set_default]
+  before_action :set_imap_setting, only: [:edit, :update, :destroy]
 
   # GET /imap_settings
   def index
@@ -9,6 +9,10 @@ class SmtpSettingsController < ApplicationController
 
   # GET /imap_settings/new
   def new
+    unless current_account.smtp_settings.present?
+      redirect_to smtp_settings_path, notice: 'Please define at least one smtp settings to read reply emails of it.'
+      return
+    end
     @imap_setting = current_account.imap_settings.new
   end
 
@@ -22,7 +26,7 @@ class SmtpSettingsController < ApplicationController
 
     respond_to do |format|
       if @imap_setting.save
-        format.html { redirect_to imap_settings_path, notice: 'Smtp setting was successfully created.' }
+        format.html { redirect_to imap_settings_path, notice: 'IMAP setting was successfully created.' }
       else
         format.html { render :new }
       end
@@ -33,7 +37,7 @@ class SmtpSettingsController < ApplicationController
   def update
     respond_to do |format|
       if @imap_setting.update(imap_setting_params)
-        format.html { redirect_to imap_settings_path, notice: 'Smtp setting was successfully updated.' }
+        format.html { redirect_to imap_settings_path, notice: 'IMAP setting was successfully updated.' }
       else
         format.html { render :edit }
       end
@@ -42,8 +46,9 @@ class SmtpSettingsController < ApplicationController
 
   # DELETE /imap_settings/1
   def destroy
+    @imap_setting.destroy
     respond_to do |format|
-      format.html { redirect_to imap_settings_url, notice: 'Smtp setting was successfully destroyed.' }
+      format.html { redirect_to imap_settings_url, notice: 'IMAP setting was successfully destroyed.' }
     end
   end
 
@@ -53,6 +58,6 @@ class SmtpSettingsController < ApplicationController
   end
 
   def imap_setting_params
-    params.require(:imap_setting).permit(:from_email, :reply_to, :provider, :address, :port, :domain, :username, :password)
+    params.require(:imap_setting).permit(:address, :port, :email, :password)
   end
 end
