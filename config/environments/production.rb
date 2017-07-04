caabebaf7e55d90eec76ec71fd136d909413872e
Sadik Ay <sadikay91@gmail.com>
2017-07-05 01:25:32 +0300

    fix default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'yourdomain.com' }
+  config.action_mailer.default_url_options = { host: 'smart-mailing.eastus.cloudapp.azure.com' }
 end
