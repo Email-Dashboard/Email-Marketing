4b6b7e4ace56f22eb3c8263c0e61543821914165
Sadik Ay <sadikay91@gmail.com>
2017-07-05 00:43:25 +0300

    fix devise actions for admin user

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,5 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 end
