316de499f813e5bfecbd75ae91e8fb5366020d06
Sadik Ay <sadikay91@gmail.com>
2017-07-22 19:13:32 +0300

    fix table views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,22 @@ module ApplicationHelper
       title
     end
   end
+
+  def source_action_icons(source, actions = [])
+    out = ''
+
+    out += (link_to edit_polymorphic_path(source), class: 'btn btn-sm btn-default btn-xs' do
+      content_tag(:i, '', class: 'fa fa-pencil')
+    end) if actions.include?('edit')
+
+    out += (link_to source, class: 'btn btn-sm btn-default btn-xs' do
+      content_tag(:i, '', class: 'fa fa-eye')
+    end) if actions.include?('show')
+
+    out += (link_to source, :method => :delete, :data => { :confirm => 'Are you sure?' }, remote: actions.include?('remote'), class: 'btn btn-sm btn-danger btn-xs' do
+      content_tag(:i, '', class: 'fa fa-trash')
+    end) if actions.include?('destroy')
+
+    out
+  end
 end
